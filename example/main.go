4c1d83fc7463ee4d3ddc2d3b67e5c3dcaac72ea9
eh2k <you@example.com>
2020-08-22 12:49:29 +0200

    gofmt

--- example/main.go
+++ example/main.go
@@ -2,18 +2,17 @@ package main
 
 import "github.com/eh2k/osdialog-go"
 
+func main() {
 
-func main(){
-
-	yes:= osdialog.ShowMessageBox(osdialog.Error, osdialog.YesNo, "Error Message!!!")
-	if yes{
+	yes := osdialog.ShowMessageBox(osdialog.Error, osdialog.YesNo, "Error Message!!!")
+	if yes {
 		osdialog.ShowMessageBox(osdialog.Info, osdialog.Ok, "Info!!!")
-	} 
-	
+	}
+
 	osdialog.ShowSaveFileDialog(".", "こんにちは", "Source:c,cpp,m;Header:h,hpp")
-	
+
 	osdialog.ShowOpenFileDialog(".", "こんにちは", "Source:c,cpp,m;Header:h,hpp")
-	
+
 	osdialog.ShowOpenDirectoryDialog("/")
 
 }
